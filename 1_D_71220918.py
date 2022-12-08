1b31a7b45e2773aa936e3b537b5b2d727460851d
Harimurti18 <117901226+Harimurti18@users.noreply.github.com>
2022-12-08 16:10:15 +0700

    Add files via upload

---
+++ 1_D_71220918.py
@@ -0,0 +1,18 @@
+susu = 20000
+permen = 500
+roti = 15000
+indomie = 3000
+vitamin = 50000
+
+
+
+
+a = int(input("Masukan jumlah uang: Rp"))
+a = "Ketik 'START' untuk memulai: "
+
+while True:
+    n = input(a)
+
+    if n == "START":
+        print("Hai")
+
